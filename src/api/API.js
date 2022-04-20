1921055a18bd373cbad12d54a91c665672f2a90d
rsammataro <ramasammataro@gmail.com>
2022-04-20 01:10:13 -0300

    token change

--- src/api/API.js
+++ src/api/API.js
@@ -12,7 +12,7 @@ const methods = {
 const token = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiUm9iZXJ0byBCZXJtZWpvIFNhbmNoZXoiLCJBbHF1aWxBci5FbWFpbCI6InJvYmVydG9iZXJtZWpvc2FuY2hlekBnbWFpbC5jb20iLCJBbHF1aWxBci5Ob21icmVVc3VhcmlvIjoiUm9iZXJ0b1NhbmNoZXoiLCJBbHF1aWxBci5Ob21icmVSb2wiOiJQYXJ0aWN1bGFyIiwiZXhwIjoxNjUwNDk4NzE3LCJpc3MiOiJBbHF1aWxBckdyb3VwIiwiYXVkIjoibG9jYWxob3N0In0.je80lNUYqL4_8oZd4TCWYd52qWk_vEaTXPtzx30WmvHXOxCPUnYMiqmbjO87xnXGJzh3IacjCI-UylzKulhlFw';
 
 const API = axios.create({
-    baseURL: 'https://localhost:5001',
+    baseURL: 'https://localhost:44380',
     headers: {
         ...(token ? { Authorization: `bearer ${token}` } : null),
     },
