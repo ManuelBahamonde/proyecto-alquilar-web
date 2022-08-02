80a4cdb9c6e2c6a699e9f2b59c8f8f23536813fb
ManuelBahamonde <manuelbahamondedev@gmail.com>
2022-08-01 23:51:05 -0300

    Initially simple implementation of Edit Profile screen *Horarios and styles yet to be added

--- src/components/Perfil/Perfil.js
+++ src/components/Perfil/Perfil.js
@@ -1,8 +1,138 @@
-import { useContext } from "react";
+import * as API from "api/API";
+import LocalidadSelect from "components/Shared/LocalidadSelect";
+import TextBox from "components/UI/TextBox";
+import { useCallback, useContext, useEffect, useState } from "react";
 import AuthContext from "storage/auth-context";
 
 const Perfil = () => {
   const authCtx = useContext(AuthContext);
-  return <h1>Bienvenido a tu perfil {authCtx.nombre} sos un usuario del tipo {authCtx.nombreRol}</h1>;
+
+  console.log(authCtx);
+
+  const [form, setForm] = useState({
+    nombre: '',
+    telefono: '',
+    email: '',
+    localidad: null,
+    direccion: '',
+    piso: null,
+  });
+  const [currentUserInfo, setCurrentUserInfo] = useState(null);
+
+  useEffect(() => {
+    if (authCtx.idUsuario) {
+      API.get(`/usuario/${authCtx.idUsuario}`)
+        .then((response) => {
+          setCurrentUserInfo(response.data);
+
+          setForm({
+            nombre: response.data.nombre,
+            telefono: response.data.telefono,
+            email: response.data.email,
+            localidad: response.data.idLocalidad,
+            direccion: response.data.direccion,
+            piso: response.data.piso,
+          });
+        });
+    }
+  }, [authCtx.idUsuario]);
+
+  const {
+    nombre,
+    telefono,
+    email,
+    localidad,
+    direccion,
+    piso
+  } = form;
+
+  const handleNombreChange = useCallback((newValue) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      nombre: newValue,
+    }));
+  }, []);
+
+  const handleTelefonoChange = useCallback((newValue) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      telefono: newValue,
+    }));
+  }, []);
+
+  const handleEmailChange = useCallback((newValue) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      email: newValue,
+    }));
+  }, []);
+
+  const handleLocalidadChange = useCallback((newValue) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      localidad: newValue,
+    }));
+  }, []);
+
+  const handleDireccionChange = useCallback((newValue) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      direccion: newValue,
+    }));
+  }, []);
+
+  const handlePisoChange = useCallback((newValue) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      piso: newValue,
+    }));
+  }, []);
+
+  return (
+    <div className="perfil-container">
+      <TextBox
+        containerClassName="search-control"
+        type="text"
+        label="Nombre"
+        value={nombre}
+        onChange={handleNombreChange}
+      />
+      <TextBox
+        containerClassName="search-control"
+        type="text"
+        label="Telefono"
+        value={telefono}
+        onChange={handleTelefonoChange}
+      />
+      <TextBox
+        containerClassName="search-control"
+        type="text"
+        label="Email"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <LocalidadSelect
+        isClearable
+        cacheOptions
+        placeholder="Rosario"
+        onChange={handleLocalidadChange}
+        value={localidad}
+      />
+      <TextBox
+        containerClassName="search-control"
+        type="text"
+        label="Direccion"
+        value={direccion}
+        onChange={handleDireccionChange}
+      />
+      <TextBox
+        containerClassName="search-control"
+        type="number"
+        label="Piso"
+        value={piso}
+        onChange={handlePisoChange}
+      />
+    </div>
+  );
 };
 export default Perfil;
